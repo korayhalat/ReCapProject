dbb7111462147802cc579dd8c8f42860a5f8b26a
Koray HALAT <korayhalat79@gmail.com>
2021-02-22 20:05:21 +0300

    Koray Halat

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,66 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId=1,BrandId=1,ColorId=1,ModelYear=2020,DailyPrice=350,Description="Kirada"},
+                new Car{CarId=2,BrandId=1,ColorId=1,ModelYear=2021,DailyPrice=450,Description="Kirada"},
+                new Car{CarId=3,BrandId=2,ColorId=1,ModelYear=2022,DailyPrice=550,Description="Kirada"},
+                new Car{CarId=4,BrandId=2,ColorId=2,ModelYear=2020,DailyPrice=350,Description="Kirada"},
+                new Car{CarId=5,BrandId=3,ColorId=2,ModelYear=2019,DailyPrice=250,Description="Kirada"},
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetById(int brandId)
+        {
+            return _cars.Where(c => c.BrandId == brandId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.ColorId = car.ColorId;
+        }
+    }
+}
